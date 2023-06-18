9255fa053b65d3cd5e062c066ad2021eba9ac4c3
iclipsvideo <iclipsvideo@gmail.com>
2023-06-18 13:28:00 +0200

    add initial ng code. to be completed

--- src/angular-vue/App.vue
+++ src/angular-vue/App.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { ngVueComponent, useNgVue } from "@jaredmcateer/ngvue3";
+import angular from "angular";
+import MyComponent from "./MyComponent.vue";
+
+const ngVue = useNgVue();
+
+angular
+  .module("vue.components", [ngVue])
+  .controller(
+    "MainController",
+    class MainController {
+      person: { firstName: string; lastName: string; description: string } = {
+        firstName: "Lilly",
+        lastName: "Ara",
+        description: "ngVue 3 supports components using the Composition API",
+      };
+
+      foo = "my-class";
+
+      updateDescription(description: string) {
+        this.person.description = description;
+      }
+    }
+  )
+  .directive(...ngVueComponent("myComponent", MyComponent));
 
 const name = ref("asdf");
 
@@ -7,7 +32,13 @@ name.value = "asdereref";
 </script>
 
 <template>
-  <div>dudjddjdj</div>
-  <div>Hello {{ name }}!</div>
   <FormKit type="text" label="First name:"></FormKit>
+  <FormKit type="text" label="Last name:"></FormKit>
+
+  <div ng-app="vue.components">
+    <div ng-controller="MainController as ctrl">
+      <input ng-model="ctrl.person.firstName" />
+      <textarea ng-model="ctrl.person.firstName"></textarea>
+    </div>
+  </div>
 </template>
