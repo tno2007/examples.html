d228bcd2a12265a37e3829f9bb531b51213febc2
iclipsvideo <iclipsvideo@gmail.com>
2023-06-18 12:15:47 +0200

    parcel and vue working

---
+++ src/angular-vue/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("asdf");
+
+name.value = "asdereref";
+</script>
+
+<template>
+  <div>dudjddjdj</div>
+  <div>Hello {{ name }}!</div>
+  <FormKit type="text" label="First name:"></FormKit>
+</template>
