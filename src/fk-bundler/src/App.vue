b26ae77a3af1e44445480e105d77b1c17a1f0c97
Lyall <lyall.vanderlinde@iriesoft.net>
2023-09-06 20:28:50 +0200

    checkpoint

---
+++ src/fk-bundler/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-green-400 w-52 h-96"></div>
+  <h1 class="text-3xl font-bold">Hello world!</h1>
+
+  <FormKit type="text" label="Enter name"></FormKit>
+</template>
+
+<script setup lang="ts"></script>
