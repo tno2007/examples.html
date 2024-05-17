1cc977d81e9955586c80b25c80eb81d418b21f5b
Lyall <lyall@iriesoft.net>
2024-05-17 11:50:11 +0200

    moved projects

--- src/angular-vue/MyComponent.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <!-- Multiple root elements allowed in Vue3 -->
-    <div>{{ salutation }}</div>
-    <button v-if="isAdmin" @click="onButtonClicked">Click Me</button>
-  </template>
--- src/my-olark/test/test.ts
+++
@@ -1,6 +0,0 @@
-const $el = document.querySelector("#app");
-
-const div = document.createElement("div");
-div.innerText = "Love";
-
-$el.appendChild(div);
