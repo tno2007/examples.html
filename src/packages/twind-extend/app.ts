d77260a504aa8355148a6205e5ba27f8d911e2f0
Lyall <lyall@iriesoft.net>
2024-06-21 07:47:26 +0200

    twind - easy!

---
+++ src/packages/twind-extend/app.ts
@@ -0,0 +1,4 @@
+import { install } from "@twind/core";
+import config from "./twind.config";
+// activate twind - must be called at least once
+install(config);
---
+++ src/packages/twind-extend/twind.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "@twind/core";
+import presetAutoprefix from "@twind/preset-autoprefix";
+import presetTailwind from "@twind/preset-tailwind";
+export default defineConfig({
+  presets: [presetAutoprefix(), presetTailwind()],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+      },
+    },
+  },
+});
